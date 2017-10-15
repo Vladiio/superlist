4679c085f7e02ec686320205a1dfb00190a7a348
Vlad M <vladpochta5@gmail.com>
2017-10-15 18:45:48 +0300

    Model for list Item and associated migration

--- lists/tests.py
+++ lists/tests.py
@@ -3,6 +3,25 @@ from django.urls import resolve
 from django.http import HttpRequest
 
 from lists.views import home_page
+from lists.models import Item
+
+
+class ItemModelTest(TestCase):
+
+    def test_saving_and_retrieving_items(self):
+        test_data = ('The first (ever) list item', 'Item the second')
+
+        for text in test_data:
+            item = Item()
+            item.text = text
+            item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), len(test_data))
+
+        for index, text in enumerate(test_data): 
+            self.assertEqual(saved_items[index].text, text)
+        
 
 
 class SmokeTest(TestCase):
