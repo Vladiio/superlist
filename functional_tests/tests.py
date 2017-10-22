80315758c9fa76e50ec2fe6af3921da6667c66db
Vlad <unix.vlad@yandex.ru>
2017-10-22 20:43:05 +0300

    Implement implicit wait for test

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -2,6 +2,7 @@ from django.test import LiveServerTestCase
 import time
 
 from selenium import webdriver
+from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.common.keys import Keys
 
 
@@ -12,14 +13,22 @@ class NewVisitorTest(LiveServerTestCase):
         self.browser = webdriver.Firefox()
         self.first_item_row = 'Buy peacock feathers'
         self.second_item_row = 'Use peacock feathers to make a fly'
+        self.MAX_WAIT = 10
 
     def tearDown(self):
         self.browser.quit()
 
-    def check_for_row_in_list_table(self, row_text):
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(row_text, [row.text for row in rows])
+    def wait_for_row_in_list_table(self, row_text):
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element_by_id('id_list_table')
+                rows = table.find_elements_by_tag_name('tr')
+                self.assertIn(row_text, [row.text for row in rows])
+            except (AssertionError, WebDriverException) as e:
+               if time.time() - start_time < self.MAX_WAIT:
+                   raise e
+               time.sleep(0.5)
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith goes to new to-do app and check out its homepage
@@ -44,8 +53,7 @@ class NewVisitorTest(LiveServerTestCase):
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list
         inputbox.send_keys(Keys.ENTER) 
-        time.sleep(1)
-        self.check_for_row_in_list_table('1: ' + self.first_item_row)
+        self.wait_for_row_in_list_table('1: ' + self.first_item_row)
 
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very methodical)
@@ -54,9 +62,8 @@ class NewVisitorTest(LiveServerTestCase):
 
         # The page updates again, and now shows both items on her list
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
-        self.check_for_row_in_list_table('1: ' + self.first_item_row)
-        self.check_for_row_in_list_table('2: ' + self.second_item_row)
+        self.wait_for_row_in_list_table('1: ' + self.first_item_row)
+        self.wait_for_row_in_list_table('2: ' + self.second_item_row)
 
         # Edith wonders whether the site will remember her list. Then she sees
         # that the site has generated a unique URL for her -- there is some
