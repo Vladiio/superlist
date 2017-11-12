fbf9a7bb6f043f0a2fdd6de6d1a08f37db31d6a2
Vlad <unix.vlad@yandex.ru>
2017-11-12 12:22:20 +0200

    Use bootstrap to improve layout

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,13 +1,13 @@
 import pdb
 import time
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 
 from selenium import webdriver
 from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.common.keys import Keys
 
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -31,7 +31,7 @@ class NewVisitorTest(LiveServerTestCase):
                    raise e
                time.sleep(0.5)
 
-    def test_can_start_a_list_for_one_user(self):
+    def can_start_a_list_for_one_user(self):
         # Edith goes to new to-do app and check out its homepage
         self.browser.get(self.live_server_url)
 
@@ -69,7 +69,7 @@ class NewVisitorTest(LiveServerTestCase):
         # Satisfied, she goes back to sleep
 
     
-    def test_multiple_users_can_start_lists_at_different_urls(self):
+    def multiple_users_can_start_lists_at_different_urls(self):
         # Edith starts a new to-do list
         self.browser.get(self.live_server_url)
         inputbox = self.browser.find_element_by_id("id_new_item")
