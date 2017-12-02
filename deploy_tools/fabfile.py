711ccdc0134ca41d9e4b767e9b57be2b2c208611
Vlad M <vladpochta5@gmail.com>
2017-12-02 14:07:47 +0200

    Create fabric.py

---
+++ deploy_tools/fabfile.py
@@ -0,0 +1,22 @@
+import random
+
+from fabric.contrib.files import append, exists, sed
+from fabric.api import env, local, run
+
+
+REPO_URL = 'https://github.com/Vladiio/superlist.git'
+
+def deploy():
+    site_folder = f'/home/{env.user}/sites/{env.host}'
+    source_folder = site_folder + '/source'
+    _create_directory_structure_if_necesssary(site_folder)
+    _get_latest_source(source_folder)
+    _update_settings(source_folder, env.host)
+    _update_virtualenv(source_folder)
+    _update_static_files(source_folder)
+    _update_database(source_folder)
+
+
+def _create_directory_structure_if_necesssary(site_folder):
+    for subfolder in ('database', 'static', 'virtualenv', 'source'):
+        run(f'mkdir -p {site_folder}/{subfolder}')
