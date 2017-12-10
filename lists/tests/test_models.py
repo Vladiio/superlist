050b591b85bfa5c176bab1811da4b925272a7758
Vlad <vladpochta5@gmail.com>
2017-12-10 19:16:17 +0200

    Adjust new list view to do model validation

--- lists/tests/test_models.py
+++ lists/tests/test_models.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from django.core.exceptions import ValidationError
 
 from lists.models import Item, List
 
@@ -21,3 +22,10 @@ class ListAndItemModelTest(TestCase):
         for index, text in enumerate(test_data):
             self.assertEqual(saved_items[index].list, list_)
             self.assertEqual(saved_items[index].text, text)
+
+    def test_cannot_save_empty_list_item(self):
+        list_ = List.objects.create()
+        item = Item(list=list_, text='')
+        with self.assertRaises(ValidationError):
+            item.save()
+            item.full_clean()
--- lists/views.py
+++ lists/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.core.exceptions import ValidationError
 
 from lists.models import Item, List
 
@@ -15,7 +16,14 @@ def view_list(request, list_id):
 def new_list(request):
     list_ = List.objects.create()
     item_text = request.POST.get('item_text', '')
-    item = Item.objects.create(text=item_text, list=list_)
+    item = Item(text=item_text, list=list_)
+    try:
+        item.full_clean()
+        item.save()
+    except ValidationError:
+        list_.delete()
+        error_msg = 'You can\'t have an empty list item'
+        return render(request, 'home.html', {'error': error_msg})
     return redirect(f'/lists/{list_.id}/')
 
 
