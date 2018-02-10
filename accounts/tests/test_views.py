a3f28d8afecb537ba32e086a883593f378fcf278
Vlad <vladpochta5@gmail.com>
2018-02-10 22:11:20 +0200

    use mocks to test django emails and messages

--- accounts/tests/test_views.py
+++ accounts/tests/test_views.py
@@ -1,7 +1,7 @@
+from unittest.mock import patch, call
+
 from django.test import TestCase
 from django.core.urlresolvers import reverse
-
-import accounts.views
 
 
 class SendLoginEmailViewTest(TestCase):
@@ -18,20 +18,17 @@ class SendLoginEmailViewTest(TestCase):
         response = self.post_data()
         self.assertRedirects(response, '/')
 
-    def test_sends_mail_to_address_from_post(self):
-        self.send_mail_called = False
-
-        def fake_send_mail(subject, body, from_email, to_list):
-            self.send_mail_called = True
-            self.subject = subject
-            self.body = body
-            self.from_email = from_email
-            self.to_list = to_list
-
-        accounts.views.send_mail = fake_send_mail
+    @patch('accounts.views.send_mail')
+    def test_sends_mail_to_address_from_post(self, mock_send_mail):
         self.post_data()
+        expected = call('Your login link for Superlists',
+                        'body text tbc', 'noreply@superlists',
+                        [self.email])
+        self.assertTrue(mock_send_mail.called)
+        self.assertEqual(expected, mock_send_mail.call_args)
 
-        self.assertTrue(self.send_mail_called)
-        self.assertEqual(self.subject, 'Your login link for Superlists')
-        self.assertEqual(self.from_email, 'noreply@superlists')
-        self.assertEqual(self.to_list, [self.email])
+    @patch('accounts.views.messages')
+    def test_adds_success_message(self, mock_messages):
+        response = self.post_data()
+        expected = call(response.wsgi_request, 'Check your email')
+        self.assertEqual(expected, mock_messages.success.call_args)
--- accounts/views.py
+++ accounts/views.py
@@ -1,14 +1,17 @@
 from django.core.mail import send_mail
+from django.contrib import messages
 
 from django.shortcuts import redirect
 
 
 def send_login_email(request):
     email = request.POST['email']
+    messages.success(request, 'Check your email')
     send_mail(
-      'Your login link for Superlists',
-      'body text tbc',
-      'noreply@superlists',
-      [email],
+        'Your login link for Superlists',
+        'Use this link to log in',
+        'noreply@superlists',
+        [email],
+
     )
     return redirect('/')
