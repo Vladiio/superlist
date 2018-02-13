c7139fc3422b6eb8037c8b3648cc4c3b488dc9c7
Vlad <vladpochta5@gmail.com>
2018-02-13 22:08:14 +0200

    add logout feature

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url
+from django.contrib.auth.views import logout
 
 from accounts.views import send_login_email, login_view
 
@@ -6,4 +7,5 @@ from accounts.views import send_login_email, login_view
 urlpatterns = [
     url(r'^send_login_email/$', send_login_email, name='send_login_email'),
     url(r'^login/$', login_view, name='login'),
+    url(r'^logout/$', logout, {'next_page': '/'}, name='logout'),
 ]
