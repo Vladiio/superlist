3d66d7929dd31918841f11c45954eef3bbf8aeeb
Vlad <vladpochta5@gmail.com>
2018-02-17 00:04:47 +0200

    precreate session, moving login checks into base

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -46,3 +46,17 @@ class FunctionalTest(StaticLiveServerTestCase):
                 if time.time() - start_time > self.MAX_WAIT:
                     raise e
                 time.sleep(0.5)
+
+    def wait_to_be_logged_in(self, email):
+        # TODO clean up wait_for stuff
+        self.wait_for(lambda: self.browser.find_element_by_link_text('Log out'))
+        self.wait_for(lambda: self.assertIn(
+            email, self.browser.find_element_by_css_selector('.navbar').text
+        ))
+
+    def wait_to_be_logged_out(self, email):
+        # TODO clean up wait_for stuff
+        self.wait_for(lambda: self.browser.find_element_by_name('email'))
+        self.wait_for(lambda: self.assertNotIn(
+            email, self.browser.find_element_by_css_selector('.navbar').text
+        ))
