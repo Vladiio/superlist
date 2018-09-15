ec45e7e4b82254620fdc903cbd9d536da7366643
Vlad <vladpochta5@gmail.com>
2018-09-15 13:10:10 +0300

    Add debug console.log for jenkins

--- lists/static/list.js
+++ lists/static/list.js
@@ -1,6 +1,7 @@
 window.Superlists = {};
 window.Superlists.initialize = function () {
     $('input[name="text"]').on('keypress', function() {
+        console.log('hiding validation error');
         $('.has-error').hide();
     });
 };
